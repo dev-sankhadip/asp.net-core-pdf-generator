afa713e29639ea6b0f15207f5c05e59b91b185f5
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-05-17 11:02:02 +0530

    pdf generator, datastorage added

---
+++ PDF_Generator/Utility/TemplateGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace PDF_Generator.Utility
+{
+    public class TemplateGenerator
+    {
+        public static string GetHTMLString()
+        {
+            var employee = DataStorage.GetAllEmployees();
+            var sb = new StringBuilder();
+            sb.Append(@"
+                        <html>
+                            <head>
+                            </head>
+                            <body>
+                                <div class='header'><h1>This is the generated PDF report!!!</h1></div>
+                                <table align='center'>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>LastName</th>
+                                        <th>Age</th>
+                                        <th>Gender</th>
+                                    </tr>");
+            foreach(var e in employee)
+            {
+                sb.AppendFormat(@"<tr>
+                                <td>{0}</td>
+                                <td>{1}</td>
+                                <td>{2}</td>
+                                <td>{3}</td>
+                                </tr>", e.Name, e.LastName, e.Age, e.Gender);
+            }
+
+            sb.Append(@"</table></body></html>");
+            return sb.ToString();
+        }
+    }
+}
